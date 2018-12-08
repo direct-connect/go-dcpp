375e984d993f12331deaebe91b9f7c095dbe405e
Denys Smirnov <denis.smirnov.91@gmail.com>
2018-12-08 03:54:12 +0200

    allow to list hubs

---
+++ hublist/hublist.go
@@ -0,0 +1,77 @@
+package hublist
+
+import (
+	"compress/bzip2"
+	"context"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+// Lists well-known hub lists.
+var Lists = []string{
+	"http://dchublist.com/hublist.xml.bz2",
+	"http://dchublist.ru/hublist.xml.bz2",
+}
+
+// Get fetches and decodes a hub list.
+func Get(ctx context.Context, url string) ([]Hub, error) {
+	var resp struct {
+		List []Hub `xml:"Hubs>Hub"`
+	}
+	err := getRaw(ctx, url, &resp)
+	return resp.List, err
+}
+
+func getRaw(ctx context.Context, url string, dst interface{}) error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("http status: %v", resp.Status)
+	}
+	zr := bzip2.NewReader(resp.Body)
+	return xml.NewDecoder(zr).Decode(dst)
+}
+
+// GetAll fetches and decodes all hub lists.
+func GetAll(ctx context.Context) ([]Hub, error) {
+	seen := make(map[string]struct{})
+	var (
+		list []Hub
+		last error
+	)
+	for _, url := range Lists {
+		hubs, err := Get(ctx, url)
+		for _, h := range hubs {
+			if _, ok := seen[h.Address]; ok {
+				continue
+			}
+			seen[h.Address] = struct{}{}
+			list = append(list, h)
+		}
+		if err != nil {
+			last = err
+		}
+	}
+	hubs, err := teGet(ctx)
+	for _, h := range hubs {
+		if _, ok := seen[h.Address]; ok {
+			continue
+		}
+		seen[h.Address] = struct{}{}
+		list = append(list, h.toHub())
+	}
+	if err != nil {
+		last = err
+	}
+	return list, last
+}
---
+++ hublist/te_home.go
@@ -0,0 +1,67 @@
+package hublist
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+const teURL = `http://www.te-home.net/?do=hublist&get=hublist.json`
+
+func teGetRaw(ctx context.Context, dst interface{}) error {
+	req, err := http.NewRequest("GET", teURL, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("http status: %v", resp.Status)
+	}
+	return json.NewDecoder(resp.Body).Decode(dst)
+}
+
+func teGet(ctx context.Context) ([]teHub, error) {
+	var resp struct {
+		List []teHub `json:"hublist"`
+	}
+	err := teGetRaw(ctx, &resp)
+	return resp.List, err
+}
+
+func (h teHub) toHub() Hub {
+	return Hub{
+		Address:     h.Address,
+		ASN:         h.Asn,
+		Bots:        h.Bots,
+		Country:     h.Country,
+		Description: h.Description,
+		Email:       h.Email,
+		Encoding:    h.Encoding,
+		Failover:    h.Failover,
+		Icon:        h.Icon,
+		Infected:    h.Infected,
+		Logo:        h.Logo,
+		Maxhubs:     h.Maxhubs,
+		Maxusers:    h.Maxusers,
+		Minshare:    uint64(h.Minshare),
+		Minslots:    h.Minslots,
+		Name:        h.Name,
+		Network:     h.Network,
+		Operators:   h.Operators,
+		Rating:      h.Rating,
+		Reliability: strconv.Itoa(h.Reliability),
+		Shared:      uint64(h.Shared),
+		Software:    h.Software,
+		Status:      h.Status,
+		Users:       h.Users,
+		Website:     h.Website,
+	}
+}
