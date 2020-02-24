a6065967fe24ca92cc9979bd67e29af6c7a8e9ec
Denys Smirnov <denis.smirnov.91@gmail.com>
2020-02-24 17:56:54 +0200

    dump version

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 const (
 	Name            = "go-dcpp"
 	HubName         = "GoHub"
-	Vers            = "0.25.0"
-	Timestamp       = "2020-02-20T00:00:00Z"
+	Vers            = "0.26.0"
+	Timestamp       = "2020-02-24T00:00:00Z"
 	TimestampFormat = "2006-01-02T15:04:05Z07:00"
 )
